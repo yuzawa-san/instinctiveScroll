d7592c240a8ddb45a68480588564802b0b5700b0
James Yuzawa <jyuzawa@u.rochester.edu>
2012-05-28 17:23:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# intuitiveScroll
+
+*version 1.1*
+
+James Yuzawa: 
+[http://www.jyuzawa.com](http://www.jyuzawa.com/)
+
+Released under MIT License
+
+A jQuery plugin for horizontal scrolling using mouse location in an element.
+Moving mouse to the right of the element will make it scroll to the right.
+Moving mouse to the left of the element will make it scroll to the left.
+Similar to mousemove implementation of [Mootools Scroller](http://demos111.mootools.net/Scroller).
+
+## Options
+
+| **key**                    | **type**             | **default value** | **description**                                                       |
+|:---------------------------|:---------------------|:------------------|:---------------------------------------------------------------------|
+| `buffer`                   | int (px)             | `75`              | The width of the buffer in the left and right of the element in which to bind the scroll event. |
+| `speed`                    | int (px/interval)    | `100`             | The maximum number of pixels to jump every time the the scroll action is fired. |
+| `interval`                 | int (ms)             | `50`              | The number of milliseconds to delay between firing the scroll action. |
+| `scroller`                 | boolean              | `true`            | Display a scroller. |
+| `locking`                  | boolean              | `false`           | Do not allow the scroll events if the mouse enters from the left or right from the outside |
+| `scrollerColor`            | int[3]               | `[100,100,100]`   | A triple of RGB color values 0-255 |
+| `scrollerOpacity`          | float                | `0.8`             | An alpha parameter from 0 to 1 |
+| `scrollerInactiveOpacity`  | float                | `0.3`             | An alpha parameter from 0 to 1 |
+
+
+## Usage
+
+Demo html:
+
+    <div id="myContainer">
+        <div id="myContent">
+            <div class="block">Wide Content Here</div>
+            <div class="block">Wide Content Here</div>
+            <div class="block">Wide Content Here</div>
+            <div class="block">Wide Content Here</div>
+            <div class="block">Wide Content Here</div>
+            <div class="block">Wide Content Here</div>
+        </div>
+    </div>
+
+with css:
+
+    #myContainer {
+        width: 500px;
+        border: 1px solid black;
+    }
+    #myContent {
+        width:1000px;
+        background: gray;
+    }
+    .block {
+        width: 150px;
+        float:left;
+        height:200px;
+        background:red;
+        color:white;
+        padding: 3px;
+        margin: 3px;
+    }
+
+with JavaScript:
+
+    $("#myContainer").instinctiveScroll();
+    
+or JavaScript with options (see below for details):
+
+    $("#myContainer").instinctiveScroll({
+        buffer: 100,
+        scroller: false,
+        scrollerOpacity: 0.95,
+    });
+    
+This usage demo is implemented in full in `demo.html`.
+
+## Notes
+
+* Window can be resized changing element size, scrolling still will work and scrollbar will properly be redrawn.
+* Cursor will indicated scrolling when mouse in the left/right buffer region of an element.
+* Speed increases quadratically (to `speed` parameter) as the mouse goes further into the buffer region.
+
+## Todo
+
+* fix random bugs with `locking` parameter
---
+++ instinctiveScroll.js
@@ -0,0 +1,148 @@
+/*
+    instinctiveScroll v1.1
+    James Yuzawa (http://www.jyuzawa.com/)
+    released under MIT License
+*/
+
+(function($) {
+    // bind it to jquery
+    $.fn.instinctiveScroll = function(options) {
+        this.each(function(){
+            return new InstinctiveScroll(this, options);
+        });
+    };
+    
+    var InstinctiveScroll = function(elem, userOpts) {
+        // plugin globals
+        var self = this;
+        var $elem = $(elem);
+        var itemWidth = $elem.width();
+        var itemHeight = $elem.height();
+        var innerWidth = $elem.children().width();
+        var hScrollTimer = null;
+        var hScrollDelta=0;
+        var entered=true;
+        var scrollerRatio=itemWidth/innerWidth;
+        
+        // options
+        var opts = {
+            buffer: 75,
+            speed: 100,
+            interval: 50,
+            scroller: true,
+            locking: false,
+            scrollerColor: [100,100,100],
+            scrollerOpacity: 0.8,
+            scrollerInactiveOpacity: 0.3,
+        };
+        
+        // overwrite defaults with user opts
+        for (var i in userOpts) {
+            if (userOpts[i] !== undefined) {
+                opts[i] = userOpts[i];
+            }
+        }
+        
+        // apply css to element
+        $elem.css({
+            "overflow":"auto",
+            "overflow-x":"hidden",
+            "overflow-y":"auto",
+            "position":"relative" 
+        });
+        
+        // scroller stuff
+        var activeScrollerColor = "rgba("+opts.scrollerColor[0]+","+opts.scrollerColor[1]+","+opts.scrollerColor[2]+","+opts.scrollerOpacity+")";
+        var inactiveScrollerColor = "rgba("+opts.scrollerColor[0]+","+opts.scrollerColor[1]+","+opts.scrollerColor[2]+","+opts.scrollerInactiveOpacity+")";
+        var scroller = $("<div/>").css({
+            "background":inactiveScrollerColor,
+            "margin":"5px",
+            "position":"absolute",
+            "border-radius":"5px",
+            "bottom": "0px",
+            "left":"0px",
+            "width":"30px",
+            "height":"8px",
+        });
+        if(opts.scroller){
+            if(scrollerRatio<=1) scroller.width(scrollerRatio*itemWidth);
+            else scroller.hide();
+            $elem.prepend(scroller);
+        }
+        
+        // function called at intervals to run scroll
+        var doScrollHorizontal = function(){
+            orig=$elem.scrollLeft();
+            calc=orig+hScrollDelta;
+            if(calc<0) calc=0; // fix left
+            if(calc>(innerWidth-itemWidth)) calc=(innerWidth-itemWidth); // fix right
+            $elem.scrollLeft(calc);
+            if(opts.scroller){ // move scroller
+                est=calc+(calc/innerWidth)*(itemWidth-14);
+                scroller.css('left',est+"px");
+            }
+            if(calc==(innerWidth-itemWidth) || calc==0){ // stop at ends of content
+                $elem.css("cursor","");
+                clearInterval(hScrollTimer);
+                hScrollTimer=null;
+            }
+        };
+        
+        // see if we are in the buffer zone in the left or right of the element
+        $elem.mousemove(function(e){
+            var parentOffset = $(this).offset(); 
+            var relX = (e.pageX - parentOffset.left);
+            rightBox = (relX > (itemWidth-opts.buffer));
+            leftBox = (relX < opts.buffer);
+            
+            orig=$elem.scrollLeft();
+            if(scrollerRatio<=1 && (rightBox || leftBox)){
+                if(rightBox){
+                    hScrollDelta=Math.pow((1-((itemWidth-relX)/opts.buffer)),2)*opts.speed;
+                    if(hScrollTimer==null && orig!=(innerWidth-itemWidth) && entered){
+                        $elem.css("cursor","e-resize");
+                        hScrollTimer=setInterval(doScrollHorizontal,opts.interval);
+                    }
+                }
+                if(leftBox){
+                    hScrollDelta=Math.pow((1-(relX/opts.buffer)),2)*-opts.speed;
+                    if(hScrollTimer==null && orig !=0 && entered){
+                        $elem.css("cursor","w-resize");
+                        hScrollTimer=setInterval(doScrollHorizontal,opts.interval);
+                    }
+                }
+            }else{
+                entered=true;
+                $elem.css("cursor","");
+                clearInterval(hScrollTimer);
+                hScrollTimer=null;
+            }
+        });
+        
+        // change scroller and lock scrolling when entering the element
+        $elem.mouseover(function(){
+            if(opts.scroller) scroller.css("background",activeScrollerColor);
+            if(opts.locking) entered=false;
+        });
+        
+        // change scroller and release scrolling when exiting the element
+        $elem.mouseout(function(){
+            $elem.css("cursor","");
+            clearInterval(hScrollTimer);
+            hScrollTimer=null;
+            if(opts.scroller) scroller.css("background",inactiveScrollerColor);
+        });
+        
+        // update metrics when window resizes in case element's frame changed
+        $(window).resize(function() {
+            itemWidth = $elem.width();
+            itemHeight = $elem.height();
+            scrollerRatio=itemWidth/innerWidth;
+            if(opts.scroller){
+                scroller.width(scrollerRatio*itemWidth);
+                if(scrollerRatio<=1) scroller.show();
+                else scroller.hide();
+            }
+        });
+    };
+})(jQuery);
